a6b3efacf4ad9e153f6f4a727ebbdbd828ff5515
Angela Lemus <angela.lemus@uabc.edu.mx>
2026-04-17 20:51:56 -0700

    metodo estatico para multiplicacion de numeros lista

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,15 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    ArrayList<Integer> numeros = new ArrayList<>();
+    numeros.add(1);
+    numeros.add(2);
+    numeros.add(3);
+    numeros.add(4);
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    System.out.println(numeros);
+    Numeros.elMultiplicador(numeros,3);
+    System.out.println(numeros);
+
+
 }
---
+++ src/Numeros.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Numeros {
+
+    public Numeros() {
+
+    }
+
+    static public ArrayList<Integer> elMultiplicador(ArrayList<Integer> numeros, int multiplicador) {
+        numeros.replaceAll(n -> n * multiplicador);
+        return numeros;
+    }
+}
